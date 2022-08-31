6b409b6b99a68efce58c4ed15fde711ce3fb855c
JP <j.p.walter@outlook.com>
2022-08-31 14:57:49 -0400

    Added styling for the game. Css is a mess imo, a lot of guess and check and Googling, but it looks pretty good. Moving on to logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Die from "./components/Die";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="gameBoard">
+      <h1 className="title">Tenzies</h1>
+      <h3 className="description">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </h3>
+      <container className="dice">
+        <Die value={1} />
+        <Die value={2} />
+        <Die value={3} />
+        <Die value={4} />
+        <Die value={5} />
+        <Die value={6} />
+        <Die value={5} />
+        <Die value={4} />
+        <Die value={3} />
+        <Die value={2} />
+      </container>
+      <button>Roll</button>
+    </main>
   );
 }
 
---
+++ src/components/Die.jsx
@@ -0,0 +1,8 @@
+function Die(props) {
+  return (
+    <div className="die-face">
+      <h2>{props.value}</h2>
+    </div>
+  );
+}
+export default Die;
